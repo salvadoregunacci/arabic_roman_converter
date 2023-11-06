cd7a0d25d13d573fb3c83ced2ba1d41bf6322ea9
Salvadore <salvadoregunacci5@gmail.com>
2023-11-06 14:29:09 +0200

    update app

--- scripts/script.js
+++ scripts/script.js
@@ -4,19 +4,21 @@
 
 const $aN = document.querySelector('#arab_num');
 const $rN = document.querySelector('#rom_num');
+const $alert = document.querySelector('.info-alert');
+const $resetBtn = document.querySelector('#reset-btn');
 
 const ROM_NUMS = {
-  "I": 1,
-  "IV": 4,
-  "V": 5,
-  "IX": 9,
-  "X": 10,
-  "L": 50,
-  "XC": 90,
-  "C": 100,
-  "D": 500,
   "CM": 900,
+  "XC": 90,
+  "IX": 9,
+  "IV": 4,
   "M": 1000,
+  "D": 500,
+  "C": 100,
+  "L": 50,
+  "X": 10,
+  "V": 5,
+  "I": 1,
 }
 
 // =========================
@@ -25,13 +27,14 @@ const ROM_NUMS = {
 
 $aN?.addEventListener("input", _calcArabToRom);
 $rN?.addEventListener("input", _calcRomToArab);
+$resetBtn?.addEventListener("click", () => _hideAlert());
 
 // =========================
 // Functions
 // =========================
 
 
-// Арабские - Римские
+// Arab-Roman
 function _calcArabToRom(e) {
   const $el = e.target;
   const _val = $el?.value;
@@ -42,57 +45,101 @@ function _calcArabToRom(e) {
 }
 
 
-// Римские - Арабские
+// Roman-Arab
 function _calcRomToArab(e) {
   const $el = e.target;
-  $el.value = $el?.value.toUpperCase();
-  let _val = $el?.value;
 
   if (!$el) return;
+  $el.value = $el?.value.toUpperCase();
 
-  const _reverseRomNums = reverseObj(ROM_NUMS);
+  if (/[А-яІіЇїЁёЄє]/g.test($el.value)) {
+    _showAlert("* Только латинcкие символы (A-Z)");
+  } else {
+    _hideAlert();
+  }
+
+  $el.value = $el.value.replace(/[^IVXLCDM]/gm, "");
+  $el.value = _correctValue($el.value);
+
+  let _val = $el.value;
+  let _isInvalid = false;
+  const _invalidPatterns = [
+    /C[DMC]{2,}/g,
+    /DM/g,
+    /IX./g,
+    /I[^IV]/g,
+    /L[DMC]/g,
+    /V[XLCDM]/g,
+    /X[DM]/g,
+    /[IXC]{4,}/g,
+    /[VLD]{2,}/g,
+    /IV./g,
+    /IC/g,
+    /VI[^I]./g,
+  ];
+
+  _invalidPatterns.forEach(_pattern => {
+    if (_val.match(_pattern)) {
+      _isInvalid = true;
+      $aN.value = NaN;
+      return;
+    }
+  });
+
+  if (_isInvalid) {
+    return;
+  }
 
   const _keys = Object.keys(ROM_NUMS);
   const _values = Object.values(ROM_NUMS);
 
-  // for (let _i = 0; _i < _keys.length; _i++) {
-  //   _val = _val.replace(new RegExp(_keys[_i], "g"), _values[_i] + " ");
-  // }
-
-  _val = _val.replace(new RegExp(_reverseRomNums["900"], "g"), ROM_NUMS.CM + " ");
-  _val = _val.replace(new RegExp(_reverseRomNums["90"], "g"), ROM_NUMS.XC + " ");
-  _val = _val.replace(new RegExp(_reverseRomNums["9"], "g"), ROM_NUMS.IX + " ");
-  _val = _val.replace(new RegExp(_reverseRomNums["4"], "g"), ROM_NUMS.IV + " ");
-  _val = _val.replace(new RegExp(_reverseRomNums["1000"], "g"), ROM_NUMS.M + " ");
-  _val = _val.replace(new RegExp(_reverseRomNums["500"], "g"), ROM_NUMS.D + " ");
-  _val = _val.replace(new RegExp(_reverseRomNums["100"], "g"), ROM_NUMS.C + " ");
-  _val = _val.replace(new RegExp(_reverseRomNums["50"], "g"), ROM_NUMS.L + " ");
-  _val = _val.replace(new RegExp(_reverseRomNums["10"], "g"), ROM_NUMS.X + " ");
-  _val = _val.replace(new RegExp(_reverseRomNums["5"], "g"), ROM_NUMS.V + " ");
-  _val = _val.replace(new RegExp(_reverseRomNums["1"], "g"), ROM_NUMS.I + " ");
+  for (let _i = 0; _i < _keys.length; _i++) {
+    _val = _val.replace(new RegExp(_keys[_i], "g"), _values[_i] + " ");
+  }
 
   _val = _val.trim();
   _val = _val.split(" ");
-
   _val = _val.map(_el => parseInt(_el));
-  _val = _val.reduce((_sum, _el) => {
-    return _sum += _el;
-  }, 0);
+  _val = _val.reduce((_sum, _el) => _sum += _el, 0);
 
   $aN.value = _val ? _val : "";
 }
 
 
-function reverseObj(_obj) {
-  const _reverse = {};
+function _showAlert(_text) {
+  if (!$alert || $alert.classList.contains("d-flex")) return;
 
-  for (const _key in _obj) {
-    if (Object.hasOwnProperty.call(_obj, _key)) {
-      const _el = _obj[_key];
+  $alert.textContent = _text;
+  $alert.classList.remove("d-none");
+  $alert.classList.add("d-flex");
+}
 
-      _reverse[_el] = _key;
-    }
+function _hideAlert() {
+  if (!$alert) return;
+
+  $alert?.classList.remove("d-flex");
+  $alert?.classList.add("d-none");
+}
+
+
+function _correctValue(_val) {
+  let _sourceVal = _val;
+
+  const _rules = {
+    "IIII": "IV",
+    "VV": "X",
+    "XXXXX": "L",
+    "LL": "C",
+    "CCCCC": "D",
+    "DD": "M",
   }
 
-  return _reverse;
+  const _k = Object.keys(_rules);
+  const _v = Object.values(_rules);
+
+  for (let _i = 0; _i < _k.length; _i++) {
+    _sourceVal = _sourceVal.replace(new RegExp(_k[_i], "g"), _v[_i]);
+  }
+
+  return _sourceVal;
 }
